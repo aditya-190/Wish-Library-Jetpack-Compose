1fdd637047a8de13cb6d5a08e4608515cc969dec
Aditya Bhardwaj <aadi.bbhardwaj@gmail.com>
2022-02-24 18:09:00 +0530

    Initial Structure to The Project.

---
+++ Wish/src/main/java/com/bhardwaj/wish/WishesStyles.kt
@@ -0,0 +1,10 @@
+package com.bhardwaj.wish
+
+enum class WishesStyles {
+    SUCCESS,
+    INFORMATION,
+    WARNING,
+    ERROR,
+    DELETE,
+    NO_INTERNET
+}
---
+++ Wish/src/main/java/com/bhardwaj/wish/ui/theme/Color.kt
@@ -0,0 +1,15 @@
+package com.bhardwaj.wish.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+
+val warningColor = Color(0xFFFBC02D)
+val errorColor = Color(0xFFD50000)
+val successColor = Color(0xFF17B978)
+val infoColor = Color(0xFF0340DA)
+val deleteColor = Color(0xFF0288D1)
+val darkBackgroundColor = Color(0xFF2F3032)
