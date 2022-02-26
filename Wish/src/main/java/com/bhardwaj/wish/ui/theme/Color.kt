f37c86ecf514e471222394b3fe5524b12b30203b
Aditya Bhardwaj <aadi.bbhardwaj@gmail.com>
2022-02-26 23:36:19 +0530

    Colors, Fonts Added to the Project.

--- Wish/src/main/java/com/bhardwaj/wish/ui/theme/Color.kt
+++ Wish/src/main/java/com/bhardwaj/wish/ui/theme/Color.kt
@@ -1,15 +1,30 @@
 package com.bhardwaj.wish.ui.theme
 
+import androidx.compose.material.Colors
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val Purple200 = Color(0xFFFBC02D)
+val Purple500 = Color(0xFF0340DA)
+val Purple700 = Color(0xFF17B978)
+val Teal200 = Color(0xFFD50000)
 
 val warningColor = Color(0xFFFBC02D)
 val errorColor = Color(0xFFD50000)
 val successColor = Color(0xFF17B978)
 val infoColor = Color(0xFF0340DA)
 val deleteColor = Color(0xFF0288D1)
-val darkBackgroundColor = Color(0xFF2F3032)
+
+val warningColorTransparent = warningColor.copy(alpha = 0.2F)
+val errorColorTransparent = errorColor.copy(alpha = 0.2F)
+val successColorTransparent = successColor.copy(alpha = 0.2F)
+val infoColorTransparent = infoColor.copy(alpha = 0.2F)
+val deleteColorTransparent = deleteColor.copy(alpha = 0.2F)
+
+val Colors.backgroundColor
+    @Composable
+    get() = if (isLight) Color(0xFFEEECEC) else Color(0xFF2F3032)
+
+val Colors.contentColor
+    @Composable
+    get() = if (isLight) Color(0xFF2F3032) else Color(0xFFEEECEC)
--- Wish/src/main/java/com/bhardwaj/wish/ui/theme/Type.kt
+++ Wish/src/main/java/com/bhardwaj/wish/ui/theme/Type.kt
@@ -1,9 +1,11 @@
 package com.bhardwaj.wish.ui.theme
 
 import androidx.compose.material.Typography
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.bhardwaj.wish.R
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -24,4 +26,14 @@ val Typography = Typography(
         fontSize = 12.sp
     )
     */
+)
+
+val helvetica = FontFamily(
+    Font(R.font.helvetica_regular, weight = FontWeight.Normal),
+    Font(R.font.helvetica_bold, weight = FontWeight.Bold)
+)
+
+val montserrat = FontFamily(
+    Font(R.font.montserrat_regular, weight = FontWeight.Normal),
+    Font(R.font.montserrat_bold, weight = FontWeight.Bold)
 )
